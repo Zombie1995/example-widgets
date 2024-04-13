8a65489db7d662c4a548b49bb3c596b62f2d0220
Zombie1995 <egorkerber1995@mail.ru>
2024-04-13 18:38:17 +0300

    set word when click

--- src/components/word-tip/ui/index.tsx
+++ src/components/word-tip/ui/index.tsx
@@ -14,13 +14,19 @@ export const WordTip = observer(() => {
       <div className="divide-y-2 divide-gray-500 outline outline-2 outline-gray-500">
         {wordTipStore.pending ? "Данные грузятся" : ""}
         {wordTipStore.words.map((word, index) => (
-          <div key={index} className="bg-white grid grid-cols-3">
-            <div className="border-r-2 border-gray-500">{word.name}</div>
-            <div>{word.fullName}</div>
+          <button
+            key={index}
+            className="bg-white grid grid-cols-3 w-full"
+            onClick={() => {
+              wordTipStore.setWord(word.name);
+            }}
+          >
+            <div className="h-full border-r-2 border-gray-500">{word.name}</div>
+            <div className="h-full">{word.fullName}</div>
             <div className="border-l-2 h-full border-gray-500">
               <img src={word.flag} alt="flag" />
             </div>
-          </div>
+          </button>
         ))}
       </div>
     </div>
